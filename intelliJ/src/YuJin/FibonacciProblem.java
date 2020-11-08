494121e7cd79d787c8e106d705df10cb2e4e898a
U-jjin <seangzui@a.ut.ac.kr>
2020-11-09 01:35:15 +0900

    no message

--- intelliJ/src/YuJin/FibonacciProblem.java
+++ intelliJ/src/YuJin/FibonacciProblem.java
@@ -1,5 +1,4 @@
-package com.practice;
-
+package YuJin;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
--- intelliJ/src/YuJin/Main.java
+++
@@ -1,4 +0,0 @@
-package YuJin;
-
-public class Main {
-}
