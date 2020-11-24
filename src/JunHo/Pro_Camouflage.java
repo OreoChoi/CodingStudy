6c3cdbe269710c265e2b0fa697abe2df535351b6
junho <chlwnsgh311@gmail.com>
2020-11-24 18:03:59 +0900

    문제 풀이  - (프로그래머스) 전화번호 목록
    주석 수정
     - (프로그래머스) 위장

--- src/JunHo/Pro_Camouflage.java
+++ src/JunHo/Pro_Camouflage.java
@@ -11,6 +11,23 @@ public class Pro_Camouflage {
     /**
      * 참고 코드
      * https://velog.io/@giraffelim/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%9C%84%EC%9E%A5
+     *
+     * 예) 모자 2 , 옷 1의 경우
+     * 1. 각 의상부위에 맞게 경우의 수를 산출
+     * 쓸 수 있는 모자 2개 + 못 쓰는 경우 1개 = 3개의 경우의 수
+     * 입을수 있는 옷 1개 + 못 입는 경우 1개 = 2개의 경우의 수
+     *
+     * 2. 총 경우의 수를 산출
+     * (3가지의 모자 패턴  * 2가지의 옷 패턴) = 6가지 패턴
+     *   1. 아무것도 안 입음
+     *   2. 모자1
+     *   3. 모자2
+     *   4. 옷 1
+     *   5. 모자1 + 옷1
+     *   6. 모자2 + 옷1
+     *
+     * 3. 아무것도 안 입은 경우를 제외하고 나머지 수를 리턴
+     *    6-1 = 5
      * */
     public static int solution(String[][] clothes) {
         int answer = 1;
@@ -21,7 +38,7 @@ public class Pro_Camouflage {
             clothesMap.put(clothe[1], clothesMap.getOrDefault(clothe[1], 0) + 1);
         }
 
-        // 경우의 수 체크 answer *= (옷 가지수 + 안 입을 경우) -> ? 흠...
+        // 경우의 수 체크 answer *= (옷 가지수 + 안 입을 경우)
         for (int val : clothesMap.values()){
             answer *= (val+1);
         }
---
+++ src/JunHo/Pro_PhoneNumberList.java
@@ -0,0 +1,35 @@
+package JunHo;
+
+import java.util.Arrays;
+
+/**
+ * jhChoi - 201124
+ * 프로그래머스 전화번호 목록
+ */
+public class Pro_PhoneNumberList {
+    public static void main(String[] args) {
+        String[] pb1 = {"119", "97674223", "1195524421"};
+        String[] pb2 = {"123", "456", "789"};
+        String[] pb3 = {"12", "123", "1235", "567", "88"};
+
+        System.out.println(solution(pb1));
+        System.out.println(solution(pb2));
+        System.out.println(solution(pb3));
+    }
+
+    public static boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book, (o1, o2) -> Integer.compare(o1.length(), o2.length()));
+
+        for (int cIdx = 0; cIdx < phone_book.length; cIdx++) {
+            String checkNumber = phone_book[cIdx];
+
+            for (int j = cIdx+1; j < phone_book.length; j++) {
+                if(phone_book[j].startsWith(checkNumber)){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
