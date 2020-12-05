2ed604f556f4e25e1200587e89a96375ce791649
cocobisc <xmfhs99@gmail.com>
2020-12-06 00:35:32 +0900

    Programmers set

---
+++ src/YoungJun/201206/n진수 게임.java
@@ -0,0 +1,25 @@
+class Solution {
+    String sy = "0123456789ABCDEF";
+	
+    private String base_to_s(int base, int n) {
+        String s = "";
+        while(n > 0) {
+            s += sy.charAt(n % base);
+            n /= base;
+        }
+        return (new StringBuffer(s)).reverse().toString();
+    }
+	
+    public String solution(int n, int t, int m, int p) {
+        String s = "0";
+        String ans = "";
+
+        for(int i = 0;; i++) {
+            if(s.length() > m * t) break;
+            s += base_to_s(n, i);
+        }
+        for(int i = 0; i < s.length() && ans.length() != t; i++)
+            if(i % m == p - 1) ans += s.charAt(i);
+        return ans;
+    }
+}
